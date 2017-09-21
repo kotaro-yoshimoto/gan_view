203fe6ef3e50e674601b1f9cd75868e7f1c630e4
Kotaro Yoshimoto <kyoushallc@gmail.com>
2017-09-21 23:00:47 +0900

    himawari

--- visualizer.py
+++ visualizer.py
@@ -19,20 +19,32 @@ import chainer.links as L
 
 import numpy as np
 
+inputimagesize = 200 #入力画像（正方想定）の１軸当りの画素数
+
 class Generator(chainer.Chain):
     def __init__(self):
         super(Generator, self).__init__(
-            l0s=L.Linear(10, 7 * 7 * 64),
+            l0s=L.Linear(10, int(inputimagesize/4) * int(inputimagesize/4) * 64),
             dc1s=L.Deconvolution2D(64, 32, 4, stride=2, pad=1),
             dc2s=L.Deconvolution2D(32, 3, 4, stride=2, pad=1),
-            bn0s=L.BatchNormalization(7 * 7 * 64),
+            bn0s=L.BatchNormalization(int(inputimagesize/4) * int(inputimagesize/4) * 64),
             bn1s=L.BatchNormalization(32),
         )
 
+#    def __call__(self, z, test=False):
+#        h = F.reshape(F.relu(self.bn0s(self.l0s(z), test=test)), (z.data.shape[0], 64, int(inputimagesize/4), int(inputimagesize/4)))
+#        h = F.relu(self.bn1s(self.dc1s(h), test=test))
+#        x = (self.dc2s(h))
+#        return x
     def __call__(self, z, test=False):
-        h = F.reshape(F.relu(self.bn0s(self.l0s(z), test=test)), (z.data.shape[0], 64, 7, 7))
-        h = F.relu(self.bn1s(self.dc1s(h), test=test))
-        x = (self.dc2s(h))
+        h = self.l0s(z)
+        h = self.bn0s(h, test=test)
+        h = F.relu(h)
+        h = F.reshape(h, (z.data.shape[0], 64, int(inputimagesize/4), int(inputimagesize/4)))
+        h = self.dc1s(h)
+        h = self.bn1s(h, test=test)
+        h = F.relu(h)
+        x = self.dc2s(h)
         return x
 
 
